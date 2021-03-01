15578e0fed95a3724fb430c4fcda08899b2a7712
Evan Shore <evandaishore12@gmail.com>
2021-03-01 15:44:13 +0900

    updated actions for choices controller. Very rough and preliminary

--- app/controllers/choices_controller.rb
+++ app/controllers/choices_controller.rb
@@ -1,19 +1,37 @@
 class ChoicesController < ApplicationController
   def index
+    @choices = Choice.all
   end
 
   def show
+    @choice = Choice.find(params[:id])
   end
 
   def new
+    @meal = Meal.find(params[:meal_id])
+    @choice = Choice.new
   end
 
   def create
+    @meal = Meal.find(params[:meal_id])
+    @choice = Choice.new(choices_params)
+    if @choice.save
+      redirect_to dashboard_path
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
-  def update  
+  def update
+    @choice = Choice.find(params[:id])
+    @choice.update(choices_params)
+    redirect_to dashboard_path
+  end
+
+  def choices_params
+    params.require(:choice).permit(:meal_id, :user_id, :rating)
   end
 end
