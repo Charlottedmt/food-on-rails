6ecf136a93fe6db4dc8771d648ba779c4c76d3d3
Ryu Watanabe <rw.ryunosuke@gmail.com>
2021-02-28 23:00:30 -0800

    Basic meals controller created

---
+++ app/controllers/meals_controller.rb
@@ -0,0 +1,28 @@
+class MealsController < ApplicationController
+
+  def index
+    @meals = Meal.all
+  end
+
+  def show
+    @meal = Meal.find(params[:id])
+  end
+
+  def new
+    @meal = Meal.new
+  end
+
+  def create
+    @meal = Meal.new(meal_params)
+    if @meal.save
+      redirect_to meals_path
+    else
+      render :new
+    end
+  end
+
+  def meal_params
+    params.require(:meal).permit(:name, :price, :calories, :protein, :fat, :carbohydrates, :sodium)
+  end
+
+end
