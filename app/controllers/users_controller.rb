3ebb87a0d99c3917b4cee02f86fc3354b1381686
Anthony Nicholson <anthony.m.nicholson@gmail.com>
2021-03-01 16:09:46 +0900

    uncommented code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,18 +5,19 @@ class UsersController < ApplicationController
   end
 
   def edit
-    
+    @user = User.find(params[:id])
+    # authorize @user
   end
 
   # not sure if we need this!
   def update
-    # @user = User.find(params[:id])
-    # # authorize @user
-    # if @user.update(user_params)
-    #   redirect_to dashboard_path 'Profile was successfully updated' #set redirect to render
-    # else
-    #   render :edit #not sure of the render yet
-    # end
+    @user = User.find(params[:id])
+    # authorize @user
+    if @user.update(user_params)
+      redirect_to dashboard_path 'Profile was successfully updated' #set redirect to render
+    else
+      render :edit #not sure of the render yet
+    end
   end
 
   def user_params
