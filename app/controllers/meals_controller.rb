f54f198ff9a35582f7df499861bd1336460661b8
Charlotte Demaret <demaret.cha@gmail.com>
2021-03-02 12:13:09 +0900

    controller meals depending on searchs

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,8 +1,13 @@
 class MealsController < ApplicationController
   skip_before_action :authenticate_user!, only: :preferences
   def index
-    @meals = Meal.all
+    if params[:query].present?
+      @meals = Movie.where("name ILIKE ?", "%#{params[:query]}%")
+    else
+      @meals = Meals.all
+    end
   end
+
 
   def show
     @meal = Meal.find(params[:id])
