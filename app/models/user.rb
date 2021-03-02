7a8752fbc8b1bb483c25ca63355c6cd06fe63d5c
Ryu Watanabe <rw.ryunosuke@gmail.com>
2021-03-01 19:26:04 -0800

    Added fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  enum status: [ :healthy, :lose_weight, :gain_weight, :gain_muscle, :low_salt ]
+    :recoverable, :rememberable, :validatable
+  enum goal: [ :healthy, :lose_weight, :gain_weight, :gain_muscle, :low_salt ]
   validates :username, presence: true, uniqueness: true
   validates :height, presence: true
   validates :weight, presence: true
-  validates :status, presence: true
+  validates :goal, presence: true
   has_many :choices
 end
