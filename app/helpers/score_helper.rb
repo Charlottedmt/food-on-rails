55f09852061257cc68a891e17d1ca0addef32e68
Ryu Watanabe <rw.ryunosuke@gmail.com>
2021-03-08 05:53:34 -0800

    Added top part (average score & last-current month score) of dashboard

---
+++ app/helpers/score_helper.rb
@@ -0,0 +1,24 @@
+module ScoreHelper
+  def get_score_class(score)
+    if score > 70
+      return "score-success"
+    elsif (51..70).cover?(score)
+      return "score-warning"
+    else
+      return "score-danger"
+    end
+  end
+  def get_score_icon(score)
+    if score > 70
+      return "caret-up"
+    elsif (51..70).cover?(score)
+      return "minus small align-middle"
+    else
+      return "caret-down"
+    end
+  end
+  def get_dashboard_score(score)
+  end
+end
+
+# <%= get_alert_class(booking.status) %>
