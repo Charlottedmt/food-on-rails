4371a435dc8f9433c27880d8f11745e8189fa308
Evan Shore <evandaishore12@gmail.com>
2021-03-09 12:12:39 +0900

    added dashboard logic w/charlotte

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -9,8 +9,31 @@ class DashboardsController < ApplicationController
       }
     @choice = Choice.find(params[:choice_id]) if params[:choice_id].present?
     @choices = policy_scope(Choice)
+    numbers_total(@choices)
   end
 
-  def calculate
+  def numbers_total(choices)
+    @calories = 0
+    @fat = 0
+    @sodium = 0
+    @carbohydrates = 0
+    @proteins = 0
+
+    choices_array = choices.each do |choice|
+      @calories += choice.meal.calories
+      @fat += choice.meal.fat
+      @sodium += choice.meal.sodium
+      @carbohydrates += choice.meal.carbohydrates
+      @proteins += choice.meal.proteins
+    end
+    equivalence(@calories, @fat, @sodium, @carbohydrates, @proteins)
+  end
+
+  def equivalence(calories, fat, sodium, carbohydrates, proteins)
+    @bike = ((calories / 300) * 30).round(1)
+    @pizza = fat / 50
+    @bread = carbohydrates / 15
+    @egg = (proteins / 6).round(1)
+    @salt = (sodium / 2.3).round(1)
   end
 end
