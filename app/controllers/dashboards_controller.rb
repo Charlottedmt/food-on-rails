d7816f2396ae54ee3c42457c85f2ceb08a2d588d
Ryu Watanabe <rw.ryunosuke@gmail.com>
2021-03-08 21:45:09 -0800

    Moved Go button in cards

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,10 +3,10 @@ class DashboardsController < ApplicationController
 
   def dashboard
     @current_position =
-      {
-        lat: params['lat'].to_f,
-        lng: params['lon'].to_f
-      }
+    {
+      lat: params['lat'].to_f,
+      lng: params['lon'].to_f
+    }
     @choice = Choice.find(params[:choice_id]) if params[:choice_id].present?
     @choices = policy_scope(Choice)
     @average_score = current_user.meals.where(choices: { created_at: Time.current.beginning_of_month..Time.current.end_of_month }).average(:score)
