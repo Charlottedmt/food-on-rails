b7f8d83940db2f906ea74301fae1f08d5df67cdf
Evan Shore <evandaishore12@gmail.com>
2021-03-10 15:02:46 +0900

    completed rough draft of protein formula

--- app/helpers/food_score_helper.rb
+++ app/helpers/food_score_helper.rb
@@ -1,34 +1,49 @@
 module FoodScoreHelper
-
-  def protein_cutoff_score
-
-      if meal.proteins > 30
-        @protein_cutoff_score + 10
-      elsif meal.proteins < 10
-        @protein_cutoff_score
-      else
-        @protein_cutoff_score + 5
-      end
-  end
-
-  def protein_ratio_score
-    @protein_ratio_score = 0
-
-    if ((meal.proteins * 4) / meal.calories) * 100 > 35
-      @protein_ratio_score + 5
-    elsif ((meal.proteins * 4) / meal.calories) * 100 < 10
-      @protein_ratio_score
+  def protein_cutoff_score(protein)
+    @cutoff_score = 0
+    if protein > 10
+      return @cutoff_score + 10
+    elsif protein < 2
+      return @cutoff_score
     else
-      @protein_ratio_score + 10
+      return @cutoff_score + 5
     end
   end
 
-  def protein_bonus_score
-
+  def protein_ratio_score(protein, calories)
+    @ratio_score = 0
+    if ((protein * 4) / calories) * 100 > 35
+      return @ratio_score + 5
+    elsif ((protein * 4) / calories) * 100 < 10
+      return @ratio_score
+    else
+      return @ratio_score + 10
+    end
   end
 
-  def proteins_score
-    protein_cutoff_score + protein_ratio_score + protein_bonus_score
+  def protein_bonus_score(protein)
+    @bonus_score = 0
+    if (2..10).cover?(protein)
+      if protein - 2 < 6
+        return @bonus_score + (protein - 2)
+      else
+        return @bonus_score + 5
+      end
+    elsif protein > 10
+      return @bonus_score + 10
+    else
+      return @bonus_score
+    end
+  end
+
+  def get_score_class(score)
+    if score > 70
+      return "score-success"
+    elsif (51..70).cover?(score)
+      return "score-warning"
+    else
+      return "score-danger"
+    end
   end
 
   def score
