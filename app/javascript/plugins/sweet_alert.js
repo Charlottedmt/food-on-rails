790febac8ee6e4b122ce61d830ecbf33a14234c5
eshore12 <evandaishore12@gmail.com>
2021-03-10 21:20:27 -0800

    Update sweet_alert.js

--- app/javascript/plugins/sweet_alert.js
+++ app/javascript/plugins/sweet_alert.js
@@ -1,15 +1,1 @@
-const sweetAlert = () =>  {
-  console.log("Hey");
-  const alert = document.querySelector(".sweetalert");
-  if (alert) {
-    Swal.fire({
-    title: 'Sweet!',
-    text: 'Modal with a custom image.',
-    imageUrl: 'https://unsplash.it/400/200',
-    imageWidth: 400,
-    imageHeight: 200,
-    imageAlt: 'Custom image',
-  }) }
-}
 
-export { sweetAlert};
