a47d10febcce8072e58717611b9440fbf769c13e
Evan Shore <evandaishore12@gmail.com>
2021-03-11 19:56:02 +0900

    updated seeds

--- db/schema.rb
+++ db/schema.rb
@@ -119,7 +119,7 @@ ActiveRecord::Schema.define(version: 2021_03_11_083104) do
     t.integer "weight"
     t.integer "goal"
     t.integer "rating"
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
